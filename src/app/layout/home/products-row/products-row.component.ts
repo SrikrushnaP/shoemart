b25dd8570d70e279fa20054a1d17a6c252d32853
Srikrushna <palsrikrushna6@gmail.com>
2024-07-08 20:08:10 +0530

    Intigrated API for froduct row in home page

--- src/app/layout/home/products-row/products-row.component.ts
+++ src/app/layout/home/products-row/products-row.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ProductService } from '../../../services/product/product.service';
 
 @Component({
   selector: 'app-products-row',
@@ -9,5 +10,19 @@ import { RouterLink } from '@angular/router';
   styleUrl: './products-row.component.css'
 })
 export class ProductsRowComponent {
+  productList: any;
+  tagName: string = "tags";
+  tagValue: string = "tranding";
 
+  constructor(private productService: ProductService ) { }
+
+  ngOnInit() {
+    this.getProductDetailsByTagName(this.tagName, this.tagValue);
+  }
+
+  getProductDetailsByTagName(tagName:any, tagValue:any){
+    this.productService.viewAllProductByParam(tagName, tagValue).subscribe((res)=>{
+      this.productList=res;
+    })
+  }
 }
--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -11,8 +11,16 @@ export class ProductService {
   constructor(private apiService: ApiService) { }
 
   viewProduct(id: any) {
-
-    console.log(this.productBaseUrl+"/"+id)
     return this.apiService.get(this.productBaseUrl+"/"+id)
+  }
+
+  /*
+    viewAllProductByParam(paramName, paramValue)
+    paramName: It will accept the any key of the product details like category, tag etc
+    paramValue: it will accept the value of categoey/ tag like for category it will be men, women, kids etc
+    By default it will accept empty value means it will show all product
+  */
+  viewAllProductByParam(paramName:any = "", paramValue:any = ""){
+    return this.apiService.get(this.productBaseUrl+"?"+paramName+"="+paramValue);
   }
 }
